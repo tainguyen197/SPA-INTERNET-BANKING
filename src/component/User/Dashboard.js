c8c9d7f065f619355e04a6074bf2b8ed05b358ac
tainguyen197 <tainguyen197.ntt@gmail.com>
2018-12-14 20:32:33 +0700

    khoi tao project, tao from dang dang

---
+++ src/component/User/Dashboard.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../../style/dashboard.css'
+
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="col-md-12 dashboard-header">
+                    <label className="usernam-lable">Xin chào, Nguyễn Trung Tài</label>
+                </div>
+                <div className="col-md-12 dashboard-body">
+                   
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
---
+++ src/component/moveMoney.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class ListMoveMoney{
+    
+}
+
+class MoveMoney extends Component {
+  render() {
+    return (
+     <div>AAA</div>
+    );
+  }
+}
+
+export default MoveMoney;
