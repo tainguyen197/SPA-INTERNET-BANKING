0d92f315ec2df58cfaad280d438bff706fdc8c29
tainguyen197 <tainguyen197.ntt@gmail.com>
2018-12-29 22:58:14 +0700

    hien thi thong tin nguoi nhan sang MoveMoeny

--- src/component/User/Dashboard/ListAccount.js
+++ src/component/User/Dashboard/ListAccount.js
@@ -3,7 +3,7 @@ import '../../../style/dashboard.css'
 import { NavLink } from 'react-router-dom'
 import { Glyphicon } from 'react-bootstrap'
 import { connect } from 'react-redux'
-import {bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux'
 import * as userAccounAction from '../../../actions/userAccountActions'
 import axios from 'axios'
 
@@ -15,37 +15,37 @@ class ListAccount extends Component {
         this.showTransaction = this.showTransaction.bind(this);
     }
 
-    showTransaction(e, account){
+    showTransaction(e, account) {
         this.props.onSelectAccountNumber(account);
     }
 
     customBalance = (number) => {
-        console.log(number);
-            var numberAccount = number.toString();
-            //console.log(numberAccount);
 
-            if (number) {
-                var array = [];
-                var i = 0;
-                while (numberAccount.length >= 3) {
-                    array[i] = numberAccount.substr(numberAccount.length - 3, 3);
-                    numberAccount = numberAccount.substr(0, numberAccount.length - 3);
-                    i++;
-                }
-                if (numberAccount.length > 0)
-                    array[i] = numberAccount.substr(0, numberAccount.length);
+        var numberAccount = number.toString();
+        //console.log(numberAccount);
 
-                //console.log(array);
-                var result = null;
-
-                result = array[array.length - 1] + '.';
-                for (var i = array.length - 2; i >= 0; i--) {
-                    result = result + array[i] + '.'
-                }
-                result = result.substr(0, result.length - 1);
-
-                return result;
+        if (number) {
+            var array = [];
+            var i = 0;
+            while (numberAccount.length >= 3) {
+                array[i] = numberAccount.substr(numberAccount.length - 3, 3);
+                numberAccount = numberAccount.substr(0, numberAccount.length - 3);
+                i++;
             }
+            if (numberAccount.length > 0)
+                array[i] = numberAccount.substr(0, numberAccount.length);
+
+            //console.log(array);
+            var result = null;
+
+            result = array[array.length - 1] + '.';
+            for (var i = array.length - 2; i >= 0; i--) {
+                result = result + array[i] + '.'
+            }
+            result = result.substr(0, result.length - 1);
+
+            return result;
+        }
     }
 
 
@@ -56,7 +56,7 @@ class ListAccount extends Component {
                 <br></br>
                 <br></br>
                 {state.map(account => {
-                    
+
                     if (account.NumberAccount !== undefined) {
                         var CustomBalance = this.customBalance(account.Balance);
                         return (
@@ -72,7 +72,7 @@ class ListAccount extends Component {
                                     <div className="col-md-1 wallet-icon timo-spendaccounticon"></div>
                                     <div className="col-md-3 ">
                                         <div className="wallet-transaction">{CustomBalance}</div>
-                                        <div onClick = {e => this.showTransaction(e, account)}><NavLink className="link btn-menu" to="/transaction">Xem các giao dịch </NavLink></div>
+                                        <div onClick={e => this.showTransaction(e, account)}><NavLink className="link btn-menu" to="/transaction">Xem các giao dịch </NavLink></div>
                                     </div>
                                     <div className="col-md-3 move-money-label">Chuyển tiền<Glyphicon glyph="glyphicon glyphicon-transfer" className="glyph-menu"></Glyphicon></div>
                                 </div>
